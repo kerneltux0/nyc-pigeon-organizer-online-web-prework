4e2372abcf4ebfff5e3f43e51fd36ff02769f50c
Ryan Sise <kerneltux0@gmail.com>
2019-03-06 18:40:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |category,details|
+    details.each do |tag,pigeons|
+      new_aspect = tag.to_s
+      pigeons.each do |name|
+        pigeon_list[name] = category
+        pigeon_list[name][category] => []
+        pigeon_list[name][category] << new_aspect
+        #binding.pry
+      end
+      #binding.pry
+    end
+    #binding.pry
+  end
+  #binding.pry
 end
