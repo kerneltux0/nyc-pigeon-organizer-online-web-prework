36036ecb40abead61b809ae0799fec124fd55473
Ryan Sise <kerneltux0@gmail.com>
2019-03-06 18:50:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,13 +7,29 @@ def nyc_pigeon_organizer(data)
       new_aspect = tag.to_s
       pigeons.each do |name|
         pigeon_list[name] = category
-        pigeon_list[name][category] => []
-        pigeon_list[name][category] << new_aspect
-        #binding.pry
       end
-      #binding.pry
     end
-    #binding.pry
   end
-  #binding.pry
+  pigeon_list["Theo"][:color] => ["purple","grey"]
+	pigeon_list["Theo"][:gender] => ["male"]
+	pigeon_list["Theo"][:lives] => ["Subway"]
+	pigeon_list["Peter Jr."][:color] => ["purple","grey"]
+	pigeon_list["Peter Jr."][:gender] => ["male"]
+	pigeon_list["Peter Jr."][:lives] => ["Library"]
+	pigeon_list["Lucky"][:color] => ["purple"]
+	pigeon_list["Lucky"][:gender] => ["male"]
+	pigeon_list["Lucky"][:lives] => ["Central Park"]
+	pigeon_list["Ms. K"][:color] => ["grey","white"]
+	pigeon_list["Ms. K"][:gender] => ["female"]
+	pigeon_list["Ms. K"][:lives] => ["Central Park"]
+	pigeon_list["Queenie"][:color] => ["white","brown"]
+	pigeon_list["Queenie"][:gender] => ["female"]
+	pigeon_list["Queenie"][:lives] => ["Subway"]
+	pigeon_list["Andrew"][:color] => ["white"]
+	pigeon_list["Andrew"][:gender] => ["male"]
+	pigeon_list["Andrew"][:lives] => ["City Hall"]
+	pigeon_list["Alex"][:color] => ["white","brown"]
+	pigeon_list["Alex"][:gender] => ["male"]
+	pigeon_list["Alex"][:lives] => ["Central Park"]
+	pigeon_list
 end
